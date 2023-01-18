38abd4771cfa2b539a69b9cb5ad233d4c2bdfd6d
pratik260620 <122601984+pratik260620@users.noreply.github.com>
2023-01-18 23:44:10 +0530

    Add files via upload

---
+++ first.py
@@ -0,0 +1,17 @@
+class Customer():
+    def __init__(self,name,age):
+        self.name= name
+        self.age= age
+
+    def intro(self):
+        print(self.name, self.age)
+
+
+C1= Customer('Pratik',32)
+C2= Customer('Priya',29)
+# print(C1)
+# print(C2)
+list=[C1,C2]
+print(list)
+for i in list:
+     i.intro()
